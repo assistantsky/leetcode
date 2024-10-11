1d8455563572e8209c12907afc958deef283f7ec
Sandeep Yadav <166513434+assistantsky@users.noreply.github.com>
2024-10-11 22:30:10 +0530

    Time: 68 ms (11.83%), Space: 51.9 MB (38.27%) - LeetHub

---
+++ 2665-counter-ii/2665-counter-ii.js
@@ -0,0 +1,33 @@
+var createCounter = function(init) {
+
+        var current = init;
+
+        return {
+
+            increment: function() {
+
+                current += 1;
+
+                return current;
+
+            },
+
+            decrement: function() {
+
+                current -= 1;
+
+                return current;
+
+            },
+
+            reset: function() {
+
+                current = init;
+
+                return current;
+
+            }
+
+        }
+
+    };
