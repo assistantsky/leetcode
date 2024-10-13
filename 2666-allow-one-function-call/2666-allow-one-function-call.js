a152bef658ac1385aae965620cb85fdd6c6efb6b
Sandeep Yadav <166513434+assistantsky@users.noreply.github.com>
2024-10-13 10:42:13 +0530

    Time: 52 ms (58.01%), Space: 48.7 MB (75.77%) - LeetHub

---
+++ 2666-allow-one-function-call/2666-allow-one-function-call.js
@@ -0,0 +1,14 @@
+var once = function(fn) {
+  let hasBeenCalled = false;
+  let result;
+
+  return function() {
+    if (!hasBeenCalled) {
+      hasBeenCalled = true;
+      result = fn.apply(this, arguments);
+      return result;
+    } else {
+      return undefined;
+    }
+  };
+};
