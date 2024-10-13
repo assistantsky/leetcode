da741b57556329b05a62bb778a71b0307888d7a2
Sandeep Yadav <166513434+assistantsky@users.noreply.github.com>
2024-10-13 22:21:01 +0530

    Time: 68 ms (28.77%), Space: 49.6 MB (42.26%) - LeetHub

---
+++ 2723-add-two-promises/2723-add-two-promises.js
@@ -0,0 +1,14 @@
+/**
+ * @param {Promise} promise1
+ * @param {Promise} promise2
+ * @return {Promise}
+ */
+var addTwoPromises = async function(promise1, promise2) {
+  try {
+    const [res1, res2] = await Promise.all([promise1, promise2]);
+    return res1 + res2;
+  } catch (error) {
+    console.error(error);
+    throw error; // Rethrow the error to maintain the behavior of propagating the error to the caller
+  }
+};
